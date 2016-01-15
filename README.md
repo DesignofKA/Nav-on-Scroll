a8fad5283ea4ad3172f6c7c41cc54e523df964ed
DesignofKA <root@kelvinasoesimac.home>
2016-01-15 17:50:31 +0000

    Working with configuration options

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-## Work In Progress :)
+Second-Nav
+==========================
+
+A quick solution to adding to add a **second navigation**
+
+Default configuration:
+
+```javascript
+$(document).ready(function() {
+    $('.scroll__section').secondNav();
+});
+```
+
+**Note** This will *slidein* the second-nav out from the top once it is past its marker & *slideout* the second-nav upwards once back before its marker.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
-jQuery(window).scroll(function () {
+(function( $ ){
+   $.fn.secondNav = function(newoptions) {
+  var options = {
+    enter: 'slideInDown',
+    exit: 'slideOutUp',
+  }
+  $.extend( true, options, newoptions );
 
-    if(jQuery(this).scrollTop() > 400)
-    {
-        if (!jQuery('.nav').hasClass('slideInDown'))
-        {
-            jQuery('.nav').stop().addClass('slideInDown').removeClass('slideOutUp animated-hold');
-        }
-    }
-    else
-    {
-        jQuery('.nav').removeClass('slideInDown').addClass('slideOutUp');
-    }
-});
+  var topHeader = jQuery(this).offset().top;
+  var height = jQuery(this).outerHeight();
+
+  jQuery(window).scroll(function () {
+
+      if(jQuery(this).scrollTop() > (topHeader + height))
+      {
+          if (!jQuery('.second-nav').hasClass(options.enter))
+          {
+              jQuery('.second-nav').stop().addClass(options.enter).removeClass(options.exit).removeClass('animated-hold');
+          }
+      }
+      else
+      {
+          jQuery('.second-nav').removeClass(options.enter).addClass(options.exit);
+      }
+  });
+  };
+})( jQuery );
